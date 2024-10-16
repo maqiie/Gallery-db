31be31f0eff3b62056cdd18d4235436252881e50
maqiie <99504010+maqiie@users.noreply.github.com>
2024-10-16 21:42:50 +0300

    updated the databse url

--- config/application.rb
+++ config/application.rb
@@ -37,8 +37,8 @@ module GalleryDb
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000', 
-                'https://gallery-17bt.vercel.app',
-                'gallery-17bt.vercel.app',
+                'https://gallery-17bt.vercel.app/',
+                
                 'https://ujenzi-gallegry-75de7aa1ebe9.herokuapp.com' 
     
         resource '*',
